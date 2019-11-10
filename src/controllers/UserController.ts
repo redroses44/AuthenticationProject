ed5c9b8bde951438f09797bbd07dfbacba1fdef1
Valeri <vakrus@ttu.ee>
2019-11-10 16:01:39 +0200

    update delete funcitonality

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import { createUser, getUsers } from '../services/UserService';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser
+} from '../services/UserService';
 
 const router = express.Router();
 
@@ -7,5 +13,11 @@ router
   .route('/')
   .get(getUsers)
   .post(createUser);
+
+router
+  .route('/:id')
+  .delete(deleteUser)
+  .get(getUser)
+  .put(updateUser);
 
 export default router;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -15,6 +15,55 @@ export const getUsers = asyncHandler(
   }
 );
 
+export const deleteUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await User.findById(req.params.id);
+
+    if (!data) {
+      return next(
+        new HttpException(404, `User with id ${req.params.id} not found`)
+      );
+    }
+    data.remove();
+    res.status(200).json({
+      success: true
+    });
+  }
+);
+
+export const updateUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let data = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!data) {
+      return next(
+        new HttpException(404, `User with id ${req.params.id} not found`)
+      );
+    }
+    res.status(200).json({
+      success: true,
+      data
+    });
+  }
+);
+
+export const getUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await User.findById(req.params.id);
+    if (!data) {
+      return next(
+        new HttpException(404, `User with id ${req.params.id} not found`)
+      );
+    }
+    res.status(200).json({
+      success: true,
+      data
+    });
+  }
+);
+
 export const createUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     let user = await User.findOne({ email: req.body.email });
@@ -25,6 +74,7 @@ export const createUser = asyncHandler(
     }
 
     user = await User.create(req.body);
+    user.save();
 
     res.status(200).json({
       success: true,
